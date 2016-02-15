bc421b7aef7a9337fd86ad2dd7d54db27a5d7034
Kamron Batman <batman@sipprs.com>
2016-02-15 14:34:02 -0800

    [Initial] Initial commit
    Initial commit of Casiopia Ultima Online Shard.

---
+++ Scripts/New/Misc/ShardInfo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Server
+{
+	public static class ShardInfo
+	{
+		public static readonly string DisplayName = "Casiopia";
+		public static readonly string ShardListingName = "Casiopia";
+		public static readonly string Website = "http://www.casiopia.net";
+		public static readonly string DevTeamEmail = "devteam@casiopia.net";
+		public static readonly string DevTeamName = "Casiopia DevTeam";
+		public static readonly string AccountConfirmationAddress = "kamron@casiopia.net";
+	}
+}
